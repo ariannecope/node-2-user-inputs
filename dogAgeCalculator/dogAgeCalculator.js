8438d3000ef518a8220f6b4ba11927941c3675c4
ariannecope <arianncope@gmail.com>
2026-06-27 15:14:49 -0500

    finish dog age calculator

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -1,10 +1,28 @@
-/* 
+ 
 
-Your task: Make a dog age calculator with Node.js!
+// Your task: Make a dog age calculator with Node.js!
 
-Follow the instructions in the README.md file in this repo.
+// Follow the instructions in the README.md file in this repo.
 
-Run this file with the node command:
-node dogAgeCalculator.js
+// Run this file with the node command:
+// node dogAgeCalculator.js
 
-*/
+// Index	Value
+// 0	path to Node
+// 1	path to your file
+// 2	dog name
+// 3	dog age
+
+let dogName = process.argv[2];
+let age = Number(process.argv[3]);
+let dogYears;
+
+if (age === 1) {
+    dogYears = 15;
+} else if (age === 2) {
+    dogYears = 24;
+}  else {
+  dogYears = 24  + (age - 2) * 5;
+}
+
+console.log(`Your dog, ${dogName}, is ${age} years old, but that's ${dogYears} years old in dog years!`);
